83ead06d713b25e695125c240870fee965782c36
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-07-31 18:40:29 +0530

    routeing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import About from "./component/About";
+import Contect from "./component/Contect";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <> <Navbar/> <Home/> </>,
+  },
+  {
+    path: "/About",
+    element: <> <Navbar/> <About/> </>,
+  },
+  {
+    path: "/Contect",
+    element: <> <Navbar/> <Contect/> </>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/component/Contect.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Contect() {
+    return <h1>contect</h1>;
+
+}
+
+export default Contect;

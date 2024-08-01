c5aba8b6e37dadd7fc2e4fda40a570e0f9190100
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-08-01 14:27:32 +0530

    link

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,27 @@
 import React from "react";
 
 function Home() {
-    return <h1>home</h1>;
+  const styles = {
+    heading: {
+      color: 'blue',
+      textAlign: 'center',
+    
+    }   
+  };
 
+  const styles1 = {
+    bg: {
+        backgroundColor: 'red',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }};
+
+  return (
+    <div style={styles1.bg}>
+      <h1 style={styles.heading}>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <div>
       <nav>
-     
+     <ul>
         <Link to="/"> <li>Home</li></Link>
         <Link to="/About"> <li>about</li></Link>
         <Link to="/Contect"> <li>contect</li></Link>
-         
+        </ul>
        
       </nav>
     </div>
